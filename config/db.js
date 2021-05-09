5b5375f742f1dc15e0ea4d715b9cce5e4bcf076f
Wasiq Abdullah <samsunggrandprimeplus056@gmail.com>
2021-05-09 16:05:35 +0500

    used mongoose.connect

--- config/db.js
+++ config/db.js
@@ -3,15 +3,21 @@ const mongoose = require('mongoose');
 
 const connectDB = () => {
 
-     mongoose.connect(process.env.DB_CONENCTION_STRING, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true })
-
-     const connection = mongoose.connection;
-
-     connection.once('open', () => {
-         console.log("DB Connection Established");   
+    //  mongoose.connect(process.env.DB_CONENCTION_STRING, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true })
+     mongoose.connect(process.env.DB_CONENCTION_STRING,{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true }).then(() => {
+        console.log("DB Connection Established");   
      }).catch(() => {
-         console.log("Connection Failed");   
-     })
+        console.log("Connection Failed");   
+    })
+
+
+    //  const connection = mongoose.connection;
+
+    //  connection.once('open', () => {
+    //      console.log("DB Connection Established");   
+    //  }).catch(() => {
+    //      console.log("Connection Failed");   
+    //  })
 
 }
 
